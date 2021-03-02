2fc8a05056537211012b3bca1f4b364ddd4fe3ae
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-03-02 16:14:43 +0200

    typescipt-type

---
+++ public/app.js
@@ -0,0 +1,24 @@
+import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/Payment.js';
+const form = document.querySelector('.new-item-form');
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul'); // thêm ! là nói element ul chắc chắn sẽ đươc tìm thấy chứ ko null
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+    list.render(doc, type.value, 'end');
+});
